529052c118e895f1fb8e9c5fa11d8fdd5c089aeb
Aziz <karimberdiyevaziz077@gmail.com>
2024-03-01 10:44:29 +0500

    finish javascript

--- src/components/WetherApp/WetherApp.jsx
+++ src/components/WetherApp/WetherApp.jsx
@@ -1,38 +1,41 @@
 import React, { useState } from "react";
 import "./WetherApp.scss";
-import clear from "../../assets/clear.png";
 import cloud from "../../assets/cloud.png";
-import drizzle from "../../assets/drizzle.png";
-import humidity1 from "../../assets/humidity.png";
-import rain from "../../assets/rain.png";
-import search__icon from "../../assets/search.png";
-import snow from "../../assets/snow.png";
+import humidity from "../../assets/humidity.png";
+import searchIcon from "../../assets/search.png";
 import wind from "../../assets/wind.png";
-const Weather = () => {
+
+const WeatherApp = () => {
   const [country, setCountry] = useState("");
 
-  let API = "9f2b65c06589e9bcc829867591a46c84";
+  const API_KEY = "9f2b65c06589e9bcc829867591a46c84";
+  const API_BASE_URL = "https://api.openweathermap.org/data/2.5/weather";
+
   const search = async () => {
-    if (!search) {
+    if (!country) {
       return;
     }
 
-    const countries = await fetch(
-      `http://api.openweathermap.org/geo/1.0/direct?q=${country}&appid=${API}`
-    ).then((res) => res.json());
+    const geoResponse = await fetch(
+      `https://api.openweathermap.org/geo/1.0/direct?q=${country}&appid=${API_KEY}`
+    );
+    const geoData = await geoResponse.json();
 
-    const { lat, lon } = countries[0];
-    let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API}&units=metric`;
-    let response = await fetch(url);
-    let data = await response.json();
-    const humm = document.getElementsByClassName("weather__hum");
-    const windd = document.getElementsByClassName("weather__speed");
-    const temp = document.getElementsByClassName("weather__temp");
-    const loc = document.getElementsByClassName("weather__loc");
-    humm[0].innerHTML = data.main.humidity;
-    windd[0].innerHTML = data.wind.speed;
-    temp[0].innerHTML = `${data.main.temp} C`;
-    loc[0].innerHTML = data.name;
+    const { lat, lon } = geoData[0];
+    const weatherUrl = `${API_BASE_URL}?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+
+    const weatherResponse = await fetch(weatherUrl);
+    const weatherData = await weatherResponse.json();
+
+    const humidityElement = document.querySelector(".weather__hum");
+    const windElement = document.querySelector(".weather__speed");
+    const temperatureElement = document.querySelector(".weather__temp");
+    const locationElement = document.querySelector(".weather__loc");
+
+    humidityElement.textContent = `${weatherData.main.humidity}%`;
+    windElement.textContent = `${weatherData.wind.speed} km/h`;
+    temperatureElement.textContent = `${weatherData.main.temp} °C`;
+    locationElement.textContent = weatherData.name;
   };
 
   return (
@@ -48,35 +51,30 @@ const Weather = () => {
                 value={country}
                 onChange={(event) => setCountry(event.target.value)}
               />
-              <div
-                className="weather__icon"
-                onClick={() => {
-                  search();
-                }}
-              >
-                <img src={search__icon} alt="asd" />
+              <div className="weather__icon" onClick={search}>
+                <img src={searchIcon} alt="Search Icon" />
               </div>
             </div>
             <div className="weather__info">
               <div className="weather__img">
-                <img src={cloud} alt="asd" />
+                <img src={cloud} alt="Cloud Icon" />
               </div>
               <div className="weather__temperature">
-                <div className="weather__temp">24°C</div>
-                <div className="weather__loc">London</div>
+                <div className="weather__temp"></div>
+                <div className="weather__loc"></div>
               </div>
               <div className="weather__data">
                 <div className="weather__el">
-                  <img src={humidity1} alt="asd" />
+                  <img src={humidity} alt="Humidity Icon" />
                   <div className="weather__item-data">
-                    <div className="weather__hum">64%</div>
+                    <div className="weather__hum"></div>
                     <div className="weather__hum-text">Humidity</div>
                   </div>
                 </div>
                 <div className="weather__el">
-                  <img src={wind} alt="asd" />
+                  <img src={wind} alt="Wind Icon" />
                   <div className="weather__item-data">
-                    <div className="weather__speed">18 km/h</div>
+                    <div className="weather__speed"></div>
                     <div className="weather__hum-text">Wind Speed</div>
                   </div>
                 </div>
@@ -89,4 +87,4 @@ const Weather = () => {
   );
 };
 
-export default Weather;
+export default WeatherApp;
